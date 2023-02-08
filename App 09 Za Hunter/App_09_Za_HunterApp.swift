71411e6dd0ff17d3d8f3bbb46b32a3d6f3d4e45f
Sora <kaithemaido@gmail.com>
2023-02-08 09:39:29 -0600

    Initial Commit

---
+++ App 09 Za Hunter/App_09_Za_HunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_09_Za_HunterApp.swift
+//  App 09 Za Hunter
+//
+//  Created by Sora on 2/8/23.
+//
+
+import SwiftUI
+
+@main
+struct App_09_Za_HunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ App 09 Za Hunter/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  App 09 Za Hunter
+//
+//  Created by Sora on 2/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
